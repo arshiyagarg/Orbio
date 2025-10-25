d29c1ec7f55c3cec29587ee285cf1480974cd95f
arshiyagarg <arshiyagarg932@gmail.com>
2025-10-25 11:27:18 +0530

    Migrated backend into a separate folder

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080" : "",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080" : process.env.BACKEND_URL,
   withCredentials: true,
 });
